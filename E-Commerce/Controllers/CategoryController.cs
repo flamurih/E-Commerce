d8e7da8eaf679372b7e1195ee69ddb60cba49cee
Flamurih <flamurhoxha1998@gmail.com>
2022-12-22 16:20:10 +0100

    Create Category Added

---
+++ E-Commerce/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
